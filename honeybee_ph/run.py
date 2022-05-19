00183797f98d2cd08000a9d5eea04f93a9433347
trevor <63176386+TfedUD@users.noreply.github.com>
2022-05-19 15:14:41 -0400

    feat: implemented run_hbjson2PHX_windows

--- honeybee_ph/run.py
+++ honeybee_ph/run.py
@@ -20,7 +20,49 @@ except ImportError as e:
 
 def _run_hbjson2PHX_windows(_hbjson, _save_file_name, _save_folder):
     # type: (str, str, str) -> tuple[str, str]
-    raise NotImplementedError
+    """Convert an HBJSON to a PHX using LBT's Python 3.7 interpreter.
+
+    Arguments:
+    ---------
+        * _hbjson (str): File path to an HBJSON file to be read in and converted to a PHX-Model.
+        * _save_file_name (str): The XML filename.
+        * _save_folder (str): The folder to save the new XML file in.
+
+    Returns:
+    --------
+        * tuple
+            - [0] (str): The path to the output XML file.
+            - [1] (str): The output xml filename.
+    """
+    # -- check the input file
+    assert os.path.isfile(_hbjson), 'No HBJSON file found at {}'.format(_hbjson)
+    directory, file_name = os.path.split(_hbjson)
+
+    # -- Resolve the file paths
+    python_dir = os.path.split(os.path.split(hb_folders.python_exe_path)[0])[0]
+    run_file_path = os.path.join(hb_folders.python_package_path,
+                                 'PHX', 'hbjson_to_wufi_xml.py')
+
+    # -- read in the hbjson file, convert to PHX
+    cmds = [hb_folders.python_exe_path,
+            run_file_path,
+            _hbjson,
+            _save_file_name,
+            _save_folder]
+    use_shell = True if os.name == 'nt' else False
+    process = subprocess.Popen(cmds,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=use_shell)
+    stdout, stderr = process.communicate()
+
+    if stderr:
+        raise Exception(stderr)
+
+    for _ in str(stdout).split('\\n'):
+        print(_)
+
+    return directory, file_name
 
 
 def _run_hbjson2PHX_unix(_hbjson, _save_file_name, _save_folder):
