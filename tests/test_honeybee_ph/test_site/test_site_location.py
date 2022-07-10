8d182802dba1a4d48585c6ea85221c9ead637e7b
PH Tools <phtools@bldgtyp.com>
2022-07-10 18:01:01 -0400

    update site tests

--- tests/test_honeybee_ph/test_site/test_site_location.py
+++ tests/test_honeybee_ph/test_site/test_site_location.py
@@ -2,14 +2,14 @@ import honeybee_ph.site
 
 
 def test_climate_location():
-    loc = honeybee_ph.site.Climate_Location()
+    loc = honeybee_ph.site.Location()
 
     assert loc
 
 
 def test_climate_location_serialization():
-    loc = honeybee_ph.site.Climate_Location()
+    loc = honeybee_ph.site.Location()
     d = loc.to_dict()
-    new_o = honeybee_ph.site.Climate_Location.from_dict(d)
+    new_o = honeybee_ph.site.Location.from_dict(d)
 
     assert new_o.to_dict() == d
