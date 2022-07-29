e62202b5914a8393d2580951f54e485a43214cdc
PH Tools <phtools@bldgtyp.com>
2022-07-29 14:49:48 -0400

    update compo

--- honeybee_grasshopper_ph/src/HBPH - Create Thermal Bridges.py
+++ honeybee_grasshopper_ph/src/HBPH - Create Thermal Bridges.py
@@ -24,7 +24,7 @@ Create a new HBPH Thermal Bridge object which can be added to an HB Model. Note 
 these thermal bridges will have no effect on the EnergyPlus/OpenStudio simualtion and 
 will only be considered when the model is exported to the PHPP/WUFI-Passive.
 -
-EM June 20, 2022
+EM July 29, 2022
 
     Args:
         _names: (List[str]) A list of the HBPH Thermal Bridge names.
@@ -38,6 +38,13 @@ EM June 20, 2022
         _lengths: (List[float]) The lengths (m). If None are provided, 
             default=0.0 m
             
+        _types: (List[str]): Input either -
+        - "15-Ambient" (default)
+        - "16-Perimeter"
+        - "17-FS/BC"
+            
+        _quantities: (List[float]): The number of times the bridge element occurs.
+            
     Returns:
         thermal_bridges_: (List[PhThermalBridge]) A list of the new HBPH
             thermal bridge objects. These can be added to a Honeybee room
@@ -46,30 +53,44 @@ EM June 20, 2022
 
 from itertools import izip_longest
 
+import scriptcontext as sc
+import Rhino as rh
+import rhinoscriptsyntax as rs
+import ghpythonlib.components as ghc
+import Grasshopper as gh
+
 from honeybee_energy_ph.construction import thermal_bridge
 from honeybee_ph_rhino.gh_compo_io import ghio_create_tb
 from honeybee_ph_utils import input_tools, preview
+from honeybee_ph_rhino.gh_compo_io import ghio_validators
+from honeybee_ph_rhino import gh_io
 
 #-------------------------------------------------------------------------------
 import honeybee_ph_rhino._component_info_
 reload(honeybee_ph_rhino._component_info_)
 ghenv.Component.Name = "HBPH - Create Thermal Bridges"
 DEV = True
-honeybee_ph_rhino._component_info_.set_component_params(ghenv, dev='JUN_20_2022')
+honeybee_ph_rhino._component_info_.set_component_params(ghenv, dev='JUL_29_2022')
 if DEV:
-    #reload(thermal_bridge)
-    #reload(ghio_create_tb)
+    reload(thermal_bridge)
+    reload(ghio_validators)
+    reload(ghio_create_tb)
     #reload(input_tools)
     reload(preview)
 
+# ------------------------------------------------------------------------------
+# -- GH Interface
+IGH = gh_io.IGH( ghdoc, ghenv, sc, rh, rs, ghc, gh )
+
 #-------------------------------------------------------------------------------
 thermal_bridges_ = []
-for i in range(len(_names)):
-    ITb = ghio_create_tb.IThermalBridge()
+for i in range(len(_geometry)):
+    ITb = ghio_create_tb.IThermalBridge(
+            IGH, input_tools.clean_get(_geometry, i)
+        )
     ITb.display_name = input_tools.clean_get(_names, i)
     ITb.psi_value = input_tools.clean_get(_psi_values, i)
     ITb.fRsi_value = input_tools.clean_get(_fRsi_values, i, 0.75)
-    ITb.length = input_tools.clean_get(_lengths, i, 0)
     ITb.group_type = input_tools.clean_get(_types, i, 15)
     ITb.quantity = input_tools.clean_get(_quantities, i, 1)
 
