9e9e5b891eacdad72cb30fe31561f271268f0570
PH Tools <phtools@bldgtyp.com>
2022-09-08 19:19:56 -0400

    fix(setup): Update build config

--- honeybee_ph/__init__.py
+++ honeybee_ph/__init__.py
@@ -3,7 +3,7 @@
 
 """Package: Honeybee-PH Classes and functions to allow for Passive-House data within Honeybee.
 
-These classes are designed to be used within a Honeybee-editing envionment, such as Rhino or 
+These classes are designed to be used within a Honeybee-editing environment, such as Rhino or 
 Grasshopper. These classes will allow for Passive House specific data to be added to certain 
 relevant Honeybee entities and logged for later use.
 """
