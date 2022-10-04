7fd2e0f76603c3cc4c09707a1639a409917f2c25
PH Tools <phtools@bldgtyp.com>
2022-10-04 12:21:21 +0200

    add initial items

---
+++ honeybee_energy_ph/properties/materials/opaque.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# -*- Python Version: 2.7 -*-
+
+"""Passive House properties for honeybee_energy.material.opaque.EnergyMaterial Objects"""
+
+try:
+    from typing import Any
+except ImportError:
+    pass  # IronPython 2.7
