0151e97943cff2ef172dc77f0173d093c2221ecb
PH Tools <phtools@bldgtyp.com>
2023-05-22 06:32:54 +0200

    fix(aperture): Add new Aperture attr
    - Support WUFI monthly shading factor attribute

--- honeybee_ph/properties/aperture.py
+++ honeybee_ph/properties/aperture.py
@@ -7,7 +7,7 @@
 try:
     from typing import Any, Dict, Optional
 except ImportError:
-    pass # IronPython 2.7
+    pass  # IronPython 2.7
 
 
 class ShadingDimensions(object):
@@ -75,8 +75,9 @@ class AperturePhProperties(object):
         self.winter_shading_factor = 0.75
         self.summer_shading_factor = 0.75
         self.shading_dimensions = None  # type: Optional[ShadingDimensions]
-        self.variant_type = '_unnamed_type_'
-        self.install_depth = 0.1016 #m
+        self.variant_type = "_unnamed_type_"
+        self.install_depth = 0.1016  # m
+        self.default_monthly_shading_correction_factor = 1.0
 
     @property
     def host(self):
@@ -92,9 +93,13 @@ class AperturePhProperties(object):
         new_properties_obj.summer_shading_factor = self.summer_shading_factor
         if self.shading_dimensions:
             new_properties_obj.shading_dimensions = self.shading_dimensions.duplicate(
-                self)
+                self
+            )
         new_properties_obj.variant_type = self.variant_type
         new_properties_obj.install_depth = self.install_depth
+        new_properties_obj.default_monthly_shading_correction_factor = (
+            self.default_monthly_shading_correction_factor
+        )
 
         return new_properties_obj
 
@@ -102,40 +107,52 @@ class AperturePhProperties(object):
         return self.__repr__()
 
     def __repr__(self):
-        return "HB-Aperture Passive House Properties: [host: {}]".format(self.host.display_name)
+        return "HB-Aperture Passive House Properties: [host: {}]".format(
+            self.host.display_name
+        )
 
     def to_dict(self, abridged=False):
-        # type: (bool) -> Dict[str, dict]
+        # type: (bool) -> Dict[str, Dict[str, Any]]
         d = {}
-        d["type"] = "AperturePhProperties" if not abridged else "AperturePhPropertiesAbridged"
+        d["type"] = (
+            "AperturePhProperties" if not abridged else "AperturePhPropertiesAbridged"
+        )
         d["id_num"] = self.id_num
         d["winter_shading_factor"] = self.winter_shading_factor
         d["summer_shading_factor"] = self.summer_shading_factor
         if self.shading_dimensions:
-            d['shading_dims'] = self.shading_dimensions.to_dict()
+            d["shading_dims"] = self.shading_dimensions.to_dict()
         d["variant_type"] = self.variant_type
         d["install_depth"] = self.install_depth
+        d[
+            "default_monthly_shading_correction_factor"
+        ] = self.default_monthly_shading_correction_factor
 
         return {"ph": d}
 
     @classmethod
     def from_dict(cls, _input_dict, host):
-        # type: (Dict, Any) -> AperturePhProperties
-        assert _input_dict["type"] == "AperturePhProperties", "Expected AperturePhProperties. Got {}.".format(
-            _input_dict["type"])
+        # type: (Dict[str, Any], Any) -> AperturePhProperties
+        assert (
+            _input_dict["type"] == "AperturePhProperties"
+        ), "Expected AperturePhProperties. Got {}.".format(_input_dict["type"])
 
         new_prop = cls(host)
         new_prop.id_num = _input_dict["id_num"]
         new_prop.winter_shading_factor = _input_dict["winter_shading_factor"]
         new_prop.summer_shading_factor = _input_dict["summer_shading_factor"]
         new_prop.variant_type = _input_dict["variant_type"]
-        
+
         # Use get to ensure backwards compatibility for now
         new_prop.install_depth = _input_dict.get("install_depth", 0.1)
 
         shading_dim_dict = _input_dict.get("shading_dims", None)
         if shading_dim_dict:
             new_prop.shading_dimensions = ShadingDimensions.from_dict(shading_dim_dict)
+
+        new_prop.default_monthly_shading_correction_factor = _input_dict.get(
+            "default_monthly_shading_correction_factor", 1.0
+        )
 
         return new_prop
 
@@ -145,12 +162,8 @@ class AperturePhProperties(object):
 
         Arguments:
         ----------
-            * _aperture_prop_dict (dict): An AperturePhPropertiesAbridged dictionary loaded from 
+            * _aperture_prop_dict (dict): An AperturePhPropertiesAbridged dictionary loaded from
                 the Aperture object itself. Unabridged.
-
-        Returns:
-        --------
-            * None
         """
 
         self.winter_shading_factor = _aperture_prop_dict["winter_shading_factor"]
@@ -158,10 +171,16 @@ class AperturePhProperties(object):
         self.variant_type = _aperture_prop_dict["variant_type"]
 
         # Use get to ensure backwards compatibility for now
-        self.install_depth = _aperture_prop_dict.get('install_depth', 0.1016) # default = 4in.
+        self.install_depth = _aperture_prop_dict.get(
+            "install_depth", 0.1016
+        )  # default = 4in.
 
         shading_dim_dict = _aperture_prop_dict.get("shading_dims", None)
         if shading_dim_dict:
             self.shading_dimensions = ShadingDimensions.from_dict(shading_dim_dict)
-        
+
+        self.default_monthly_shading_correction_factor = _aperture_prop_dict.get(
+            "default_monthly_shading_correction_factor", 1.0
+        )
+
         return None
