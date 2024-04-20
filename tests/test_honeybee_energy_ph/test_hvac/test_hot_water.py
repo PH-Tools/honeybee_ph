cfece993604e2f3b566e5d7bcf0f7fb34f9960f2
PH Tools <phtools@bldgtyp.com>
2024-04-20 12:14:04 -0400

    Create test_hot_water.py

---
+++ tests/test_honeybee_energy_ph/test_hvac/test_hot_water.py
@@ -0,0 +1,93 @@
+import pytest
+from honeybee_energy_ph.hvac.hot_water import (
+    PhPipeDiameter,
+    PhPipeMaterial,
+    PhPipeSegment,
+    PhPipeElement,
+    PhPipeBranch,
+    PhPipeTrunk,
+    PhSHWTankType,
+    PhSHWTank,
+    PhHotWaterHeater,
+    PhSHWHeaterElectric,
+    PhSHWHeaterBoiler,
+    PhSHWHeaterBoilerWood,
+    PhSHWHeaterDistrict,
+    PhSHWHeaterHeatPump,
+    PhSHWHeaterBuilder,
+)
+
+
+def test_PipeDiameter_raises_exception():
+    with pytest.raises(Exception):
+        PhPipeDiameter()
+
+
+def test_PipeMaterial_raises_exception():
+    with pytest.raises(Exception):
+        PhPipeMaterial()
+
+
+def test_PipeSegment_raises_exception():
+    with pytest.raises(Exception):
+        PhPipeSegment()
+
+
+def test_PipeElement_raises_exception():
+    with pytest.raises(Exception):
+        PhPipeElement()
+
+
+def test_PipeBranch_raises_exception():
+    with pytest.raises(Exception):
+        PhPipeBranch()
+
+
+def test_PipeTrunk_raises_exception():
+    with pytest.raises(Exception):
+        PhPipeTrunk()
+
+
+def test_SHWTankType_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWTankType()
+
+
+def test_SHWTank_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWTank()
+
+
+def test_HotWaterHeater_raises_exception():
+    with pytest.raises(Exception):
+        PhHotWaterHeater()
+
+
+def test_SHWHeaterElectric_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWHeaterElectric()
+
+
+def test_SHWHeaterBoiler_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWHeaterBoiler()
+
+
+def test_SHWHeaterBoilerWood_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWHeaterBoilerWood()
+
+
+def test_SHWHeaterDistrict_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWHeaterDistrict()
+
+
+def test_SHWHeaterHeatPump_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWHeaterHeatPump()
+
+
+def test_SHWHeaterBuilder_raises_exception():
+    with pytest.raises(Exception):
+        PhSHWHeaterBuilder()
