deb9c22d7a24e9360cdc790f4da7a21998bbb13d
PH Tools <phtools@bldgtyp.com>
2024-05-03 08:22:23 -0400

    Add Transforms to SpaceVolume

--- honeybee_ph/space.py
+++ honeybee_ph/space.py
@@ -303,7 +303,7 @@ class SpaceVolume(_base._Base):
         super(SpaceVolume, self).__init__()
         self.avg_ceiling_height = 2.5  # m
         self.floor = SpaceFloor()
-        self.geometry = list()  # type: List[geometry3d.face.Face3D]
+        self.geometry = []  # type: List[geometry3d.face.Face3D]
 
     @property
     def net_volume(self):
@@ -343,18 +343,21 @@ class SpaceVolume(_base._Base):
         """Delete all the geometry from the SpaceVolume."""
         self.geometry = []
 
-    def duplicate(self):
-        # type: () -> SpaceVolume
+    def duplicate(self, _include_floor=True):
+        # type: (bool) -> SpaceVolume
         new_volume = SpaceVolume()
 
         new_volume.identifier = self.identifier
         new_volume.display_name = self.display_name
         new_volume.user_data = self.user_data
-
         new_volume.avg_ceiling_height = self.avg_ceiling_height
-        new_volume.floor = self.floor.duplicate()
+
+        if _include_floor:
+            new_volume.floor = self.floor.duplicate()
+
         if self.geometry:
             new_volume.geometry = [geo.duplicate() for geo in self.geometry]
+
         return new_volume
 
     def to_dict(self, include_mesh=False, *args, **kwargs):
@@ -394,30 +397,87 @@ class SpaceVolume(_base._Base):
 
         return new_obj
 
-    def scale(self, factor, origin=None):
-        # type: (float, Optional[geometry3d.Point3D]) -> None
-        """Scale the volume and all the geometry of the volume by a specified factor.
+    def move(self, moving_vec3D):
+        # type: (geometry3d.Vector3D) -> SpaceVolume
+        """Move the SpaceVolume along a vector.
 
-        Arguments:
-        ----------
-            * factor (float): The scale factor
-            * origin (Optional[geometry3d.Point3D]): default=None, A ladybug_geometry
-                Point3D representing the origin from which to scale. If None,
-                it will be scaled from the World origin (0, 0, 0).
-
+        Args:
+            moving_vec3D: A Vector3D with the direction and distance to move the ray.
         Returns:
-        --------
-            * None
+            A new SpaceVolume object with the move applied.
         """
+        dup_volume = self.duplicate(_include_floor=False)
+        dup_volume.geometry = [f.move(moving_vec3D) for f in self.geometry]
+        dup_volume.floor = self.floor.move(moving_vec3D)
+        return dup_volume
 
-        original_geometry = copy(self.geometry)
-        self.clear_volume_geometry()
-        for srfc in original_geometry:
-            self.geometry.append(srfc.scale(factor, origin))
+    def rotate(self, axis_vec3D, angle_degrees, origin_pt3D):
+        # type: (geometry3d.Vector3D, float, geometry3d.Point3D) -> SpaceVolume
+        """Rotate the SpaceVolume by a certain angle around an axis_vec3D and origin_pt3D.
 
-        self.avg_ceiling_height = self.avg_ceiling_height * factor
+        Right hand rule applies:
+        If axis_vec3D has a positive orientation, rotation will be clockwise.
+        If axis_vec3D has a negative orientation, rotation will be counterclockwise.
 
-        self.floor.scale(factor, origin)
+        Args:
+            axis_vec3D: A Vector3D axis_vec3D representing the axis_vec3D of rotation.
+            angle_degrees: An angle for rotation in degrees.
+            origin_pt3D: A Point3D for the origin_pt3D around which the object will be rotated.
+        Returns:
+            A new SpaceVolume object with the rotation applied.
+        """
+        dup_volume = self.duplicate(_include_floor=False)
+        dup_volume.geometry = [f.rotate(axis_vec3D, angle_degrees, origin_pt3D) for f in self.geometry]
+        dup_volume.floor = self.floor.rotate(axis_vec3D, angle_degrees, origin_pt3D)
+        return dup_volume
+
+    def rotate_xy(self, angle_degrees, origin_pt3D):
+        # type: (float, geometry3d.Point3D) -> SpaceVolume
+        """Rotate the SpaceVolume counterclockwise in the XY plane by a certain angle.
+
+        Args:
+            angle_degrees: An angle in degrees.
+            origin_pt3D: A Point3D for the origin_pt3D around which the object will be rotated.
+        Returns:
+            A new SpaceVolume object with the rotation applied.
+        """
+        dup_volume = self.duplicate(_include_floor=False)
+        dup_volume.geometry = [f.rotate_xy(angle_degrees, origin_pt3D) for f in self.geometry]
+        dup_volume.floor = self.floor.rotate_xy(angle_degrees, origin_pt3D)
+        return dup_volume
+
+    def reflect(self, normal_vec3D, origin_pt3D):
+        # type: (geometry3d.Vector3D, geometry3d.Point3D) -> SpaceVolume
+        """Reflected the SpaceVolume across a plane with the input normal vector and origin_pt3D.
+
+        Args:
+            normal_vec3D: A Vector3D representing the normal vector for the plane across
+                which the line segment will be reflected. THIS VECTOR MUST BE NORMALIZED.
+            origin_pt3D: A Point3D representing the origin_pt3D from which to reflect.
+        Returns:
+            A new SpaceVolume object with the reflection applied.
+        """
+        dup_volume = self.duplicate(_include_floor=False)
+        dup_volume.geometry = [f.reflect(normal_vec3D, origin_pt3D) for f in self.geometry]
+        dup_volume.floor = self.floor.reflect(normal_vec3D, origin_pt3D)
+        return dup_volume
+
+    def scale(self, scale_factor, origin_pt3D=None):
+        # type: (float, Optional[geometry3d.Point3D]) -> SpaceVolume
+        """Scale the SpaceVolume by a factor from an origin_pt3D point.
+
+        Args:
+            scale_factor: A number representing how much the line segment should be scaled.
+            origin_pt3D: A Point3D representing the origin_pt3D from which to scale.
+                If None, it will be scaled from the World origin_pt3D (0, 0, 0).
+        Returns:
+            A new SpaceVolume object with the scaling applied.
+        """
+        dup_volume = self.duplicate(_include_floor=False)
+        dup_volume.geometry = [f.scale(scale_factor, origin_pt3D) for f in self.geometry]
+        dup_volume.avg_ceiling_height = self.avg_ceiling_height * scale_factor
+        dup_volume.floor = self.floor.scale(scale_factor, origin_pt3D)
+        return dup_volume
 
     def __str__(self):
         return "{}()".format(self.__class__.__name__)
@@ -650,7 +710,7 @@ class Space(_base._Base):
             A new Space object with the reflection applied.
         """
         dup_space = self.duplicate(self.host, _include_volumes=False)
-        dup_space.add_new_volumes([vol.reflect(normal_vec3D) for vol in self.volumes])
+        dup_space.add_new_volumes([vol.reflect(normal_vec3D, origin_pt3D) for vol in self.volumes])
         dup_space.properties.reflect(normal_vec3D)
         return dup_space
 
