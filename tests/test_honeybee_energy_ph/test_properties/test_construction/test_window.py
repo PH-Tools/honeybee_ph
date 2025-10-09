7f0941f4c9f22287ce1988b26c197c37723a5170
PH Tools <phtools@bldgtyp.com>
2025-10-09 17:06:06 -0400

    Update test_window.py

--- tests/test_honeybee_energy_ph/test_properties/test_construction/test_window.py
+++ tests/test_honeybee_energy_ph/test_properties/test_construction/test_window.py
@@ -45,3 +45,13 @@ def test_default_window_construction_shade_properties_wrong_type_from_dict():
     }
     with pytest.raises(window.WindowConstructionPhProperties_FromDictError):
         s1 = window.WindowConstructionPhProperties.from_dict(wrong_dict, None)
+
+
+def test_window_construction_with_ph_frame_duplicate():
+    frame = PhWindowFrame("frame")
+    s1 = window.WindowConstructionPhProperties("test_shade_prop")
+    s1.ph_frame = frame
+    s2 = s1.duplicate()
+
+    assert s1.to_dict() == s2.to_dict()
+    assert s1.ph_frame is not s2.ph_frame
