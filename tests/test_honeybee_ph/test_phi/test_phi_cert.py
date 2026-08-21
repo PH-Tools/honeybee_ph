b26042a398b80cb57c5ffe83939bdebe19acb9e5
Steven Colasanti <scola916@aol.com>
2026-08-21 21:40:30 +0000

    I am using vscode, so I will stick with that, thanks!
    I have the dependencies working ok now. I am still getting issues in PHX when I tried to run the develooper 'PHX_to_PHPP' test in the command line, but thats for a different PR
    
    I use them occasionally for other things, but have intentionally not used them on these first commits because I wanted to have to dig into the code and try to understand it on my own at first. I think thats a good idea to use it on the environment stuff because im not as interested in understanding that haha
    
    I added tests for rejection cases with invalid inp
    uts like "1-BANANA" and "11". Specifically one tests an input where .number is valid but the string isn't. the other test is for a numeric value that hits "_" phpp placeholders
    
    I added tests to check the passes silently cases, comparing defaults to "", 0, and None. 0 does not behave the same as the other two and fails, so I commented it out it needs further investigation.
    
    I did not add these new tests for V9 but we should make a new issue for those specifically.
    
    I ran black formatting but I had to use the GUI. When I try 'python -m black' in command line it give me the following and I haven't found a way to make it go
    'Usage: python -m black [OPTIONS] SRC ...
    
    One of 'SRC' or 'code' is required.'
    
    Thanks so much, I appreciate all the help and patience getting this here.
    
    modified:   tests/test_honeybee_ph/test_phi/test_phi_cert.py

--- tests/test_honeybee_ph/test_phi/test_phi_cert.py
+++ tests/test_honeybee_ph/test_phi/test_phi_cert.py
@@ -2,7 +2,7 @@ from ladybug_geometry.geometry3d.plane import Plane
 from ladybug_geometry.geometry3d.pointvector import Point3D, Vector3D
 
 from honeybee_ph import phi
-
+import pytest
 
 def test_phi_cert():
     phi_cert = phi.PhiCertification()
@@ -84,19 +84,19 @@ def test_phi_cert_serialization_default_v10():
     phi_cert = phi.PhiCertification(phpp_version=10)
     phi_cert_attributes = phi_cert.attributes
 
-    d = phi_cert.to_dict()
-    new_obj = phi_cert.from_dict(d)
+    default_cert = phi_cert.to_dict()
+    new_obj = phi.PhiCertification.from_dict(default_cert)
 
-    assert new_obj.attributes.phpp_version == phi_cert_attributes.phpp_version
+    assert new_obj.attributes.phpp_version == phi_cert_attributes.phpp_version #test default serialization
 
 
 def test_phi_cert_serialization_customized_v10():
     phi_cert = phi.PhiCertification(phpp_version=10)
 
     phi_cert_attributes = phi_cert.attributes  # type: phi.PHPPSettings10
-    phi_cert_attributes.building_use_type = "10"  # should fail on invalid entry like 11
+    phi_cert_attributes.building_use_type = "10"  # invalid input 11 test in test_invalid_entry_in_phi_cert_v10
     phi_cert_attributes.ihg_type = (
-        "1-USER-DEFINED"  # should fail with invalid input like "1-BANANA", but currently passes
+        "1-USER-DEFINED"  # invalid input eg "1-BANANA" test in test_invalid_entry_in_phi_cert_v10 
     )
     phi_cert_attributes.certification_class = "10"
     phi_cert_attributes.certification_type = "10"
@@ -104,8 +104,8 @@ def test_phi_cert_serialization_customized_v10():
     phi_cert_attributes.retrofit_type = "1"
     phi_cert_attributes.tfa_override = 436.89
 
-    d = phi_cert.to_dict()
-    new_obj = phi_cert.from_dict(d)  # round trip to check serialization survival
+    default_cert = phi_cert.to_dict()
+    new_obj = phi.PhiCertification.from_dict(default_cert)  # round trip to check serialization survival
 
     assert new_obj.attributes.phpp_version == phi_cert_attributes.phpp_version
     assert new_obj.attributes.building_use_type == phi_cert_attributes.building_use_type
@@ -115,3 +115,33 @@ def test_phi_cert_serialization_customized_v10():
     assert new_obj.attributes.primary_energy_type == phi_cert_attributes.primary_energy_type
     assert new_obj.attributes.retrofit_type == phi_cert_attributes.retrofit_type
     assert new_obj.attributes.tfa_override == phi_cert_attributes.tfa_override
+
+
+def test_invalid_entry_in_phi_cert_v10():
+    phi_cert = phi.PhiCertification(phpp_version=10)
+    phi_cert_attributes = phi_cert.attributes  # type: phi.PHPPSettings10
+
+    with pytest.raises(Exception):
+        phi_cert_attributes.building_use_type = "11"  # test invalid input fails
+
+    with pytest.raises(Exception):
+        phi_cert_attributes.ihg_type = "1-BANANA"  # test valid .number but invalid string fails
+
+def test_phi_cert_none_type_v10():
+    phi_cert = phi.PhiCertification(phpp_version=10)
+    phi_cert_attributes = phi_cert.attributes
+
+    default_cert = phi_cert.to_dict()
+
+    phi_cert_attributes.ihg_type = ""
+    empty_cert = phi_cert.to_dict()
+    assert empty_cert == default_cert #empty value returns default value
+
+    '''phi_cert_attributes.ihg_type = "0"
+    zero_cert = phi_cert.to_dict()
+    assert zero_cert == default_cert''' #Currently Fails: zero value not behaving the same as empty string and None type inputs. needs further investigation 
+    
+    phi_cert_attributes.ihg_type = None
+    none_cert = phi_cert.to_dict()
+
+    assert none_cert == default_cert #none value returns default value
